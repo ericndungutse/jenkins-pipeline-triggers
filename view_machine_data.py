c09e8c8d16f6b68ed7996451be3e266bd6d52b02
ericndungutse <dav.ndungutse@gmail.com>
2026-01-22 17:31:59 +0200

    update

--- view_machine_data.py
+++ view_machine_data.py
@@ -1,8 +1,10 @@
 import socket
+import os
 
 def main():
     hostname = socket.gethostname()
     print(f"Name of the server: {hostname}")
+    print(f"Current working directory: {os.getcwd()}")
 
 if __name__ == "__main__":
     main()
